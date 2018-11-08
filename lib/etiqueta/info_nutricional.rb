a2d19706c077f237bdf729879ba694df5b19b3f5
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-11-08 23:47:30 +0000

    Espectativa + método to_s

--- lib/etiqueta/info_nutricional.rb
+++ lib/etiqueta/info_nutricional.rb
@@ -1,6 +1,21 @@
 class InfoNutricional
 	attr_reader :nombre, :porcion, :grasa, :grasa_saturada, :grasa_monoinsaturada, :grasa_poliinsaturada, :carbohidratos, :azucar, :polialcoholes, :almidon, :fibra, :proteina, :sal
+	
 	def initialize(nombre, porcion, grasa, grasa_saturada, grasa_monoinsaturada, grasa_poliinsaturada, carbohidratos, azucar, polialcoholes, almidon, fibra, proteina, sal)
 		@nombre, @porcion, @grasa, @grasa_saturada, @grasa_monoinsaturada, @grasa_poliinsaturada, @carbohidratos, @azucar, @polialcoholes, @almidon, @fibra, @proteina, @sal = nombre, porcion, grasa, grasa_saturada, grasa_monoinsaturada, grasa_poliinsaturada, carbohidratos, azucar, polialcoholes, almidon, fibra, proteina, sal
+	end
+
+	def valor_energetico
+		(@grasa*9+@carbohidratos*4+@proteina*4+@fibra*2+@sal*6).round
+	end
+
+	def to_s
+		filas = [["Valor energético","#{(valor_energetico*4.1868).round}kJ/ #{valor_energetico}kcal", "#{(valor_energetico*4.1686*@porcion/100).round}kJ/ #{(valor_energetico*@porcion/100).round}kcal"], 
+	   ["Grasas totales\n -Saturadas\n -Monoinsaturada\n -Poliinsaturada", "#{@grasa}g\n#{@grasa_saturada}g\n#{@grasa_monoinsaturada}g\n#{@grasa_poliinsaturada}g", "#{@grasa*@porcion/100}g\n#{@grasa_saturada*@porcion/100}g\n#{@grasa_monoinsaturada*@porcion/100}g\n#{@grasa_poliinsaturada*@porcion/100}g"], 
+	   ["Hidratos de carbono\n -Azúcares\n -Polialcoholes\n -Almidón","#{@carbohidratos}g\n#{@azucar}g\n#{@polialcoholes}g\n#{@almidon}g","#{@carbohidratos*@porcion/100}g\n#{@azucar*@porcion/100}g\n#{@polialcoholes*@porcion/100}g\n#{@almidon*@porcion/100}g"], 
+	   ["Fibra","#{@fibra}g","#{@fibra*@porcion/100}g"], 
+	   ["Proteina","#{@proteina}g","#{@proteina*@porcion/100}g"], 
+	   ["Sal","#{@sal}g","#{@sal*@porcion/100}g"]]
+tabla = Terminal::Table.new :title => "Galletas integrales", :headings => ['', 'Cantidad por 100g', "Cantidad por porción (20g)"], :rows => filas
 	end
 end
--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -1,5 +1,6 @@
 require 'etiqueta'
 require 'spec_helper'
+require 'terminal-table'
 RSpec.describe Etiqueta do
   it "has a version number" do
     expect(Etiqueta::VERSION).not_to be nil
@@ -8,14 +9,13 @@ RSpec.describe Etiqueta do
   it "does something useful" do
     expect(true).to eq(true)
   end
-  
+  before :each do
+                @etiqueta = InfoNutricional.new("Galletas integrales", 20, 12.6, 1.2, 9.9, 1.5, 70.0, 21.0, 0.0, 0.0, 7.5, 7.3, 0.003)
+        end
   context "# Probando el constructor" do
                 it "Espectativa de que existe un constructor" do
 			prueba = InfoNutricional.new("Galletas integrales", 20, 12.6, 1.2, 9.9, 1.5, 70.0, 21.0, 0.0, 0.0, 7.5, 7.3, 0.003)
                 end
-		before :each do
-			@etiqueta = InfoNutricional.new("Galletas integrales", 20, 12.6, 1.2, 9.9, 1.5, 70.0, 21.0, 0.0, 0.0, 7.5, 7.3, 0.003)
-		end	
 		it "Espectativa de que los elementos existen" do
 			expect(@etiqueta.nombre).not_to be nil
 			expect(@etiqueta.porcion).not_to be nil
@@ -70,6 +70,18 @@ RSpec.describe Etiqueta do
 		it "Se almacena correctamente la sal" do
 			expect(@etiqueta.sal).to eq(0.003)
                 end
+  end
+  context "# Métodos adicionales" do
+	it "Espectativa para método valor_energetico" do
+		expect(@etiqueta.valor_energetico).to eq(438)
+	end
+  end
+  context "# Métodos para mostrar por pantalla" do
+	  it "Espectativa para método to_s, tabla formateada" do
+		  filas = [["Valor energético","1834kJ/ 438kcal", "365kJ/ 87kcal"], ["Grasas totales\n -Saturadas\n -Monoinsaturada\n -Poliinsaturada", "12.6g\n1.2g\n9.9g\n1.5g", "2.52g\n0.24g\n1.98g\n0.3g"], ["Hidratos de carbono\n -Azúcares\n -Polialcoholes\n -Almidón","70.0g\n21.0g\n0.0g\n0.0g","14.0g\n4.2g\n0.0g\n0.0g"], ["Fibra","7.5g","1.5g"], ["Proteina","7.3g","1.46g"], ["Sal","0.003g","0.0006g"]]
+		tabla = Terminal::Table.new :title => "Galletas integrales", :headings => ['', 'Cantidad por 100g', "Cantidad por porción (20g)"], :rows => filas
+		expect("#{@etiqueta.to_s}").to eq("#{tabla}")
+	  end
   end
 end
 
