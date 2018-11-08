3b1ac34b5e86463080a3163974ac1c264a9ec462
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-11-08 00:54:01 +0000

    Corrección de espectativas

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -16,8 +16,20 @@ RSpec.describe Etiqueta do
 		before :each do
 			@etiqueta = InfoNutricional.new("Galletas integrales", 20, 12.6, 1.2, 9.9, 1.5, 70.0, 21.0, 0.0, 0.0, 7.5, 7.3, 0.003)
 		end	
-		it "Espectativa de que el nombre no es nulo" do
+		it "Espectativa de que los elementos existen" do
 			expect(@etiqueta.nombre).not_to be nil
+			expect(@etiqueta.porcion).not_to be nil
+			expect(@etiqueta.grasa).not_to be nil
+			expect(@etiqueta.grasa_saturada).not_to be nil
+			expect(@etiqueta.grasa_monoinsaturada).not_to be nil
+			expect(@etiqueta.grasa_poliinsaturada).not_to be nil
+			expect(@etiqueta.carbohidratos).not_to be nil
+			expect(@etiqueta.azucar).not_to be nil
+			expect(@etiqueta.polialcoholes).not_to be nil
+			expect(@etiqueta.almidon).not_to be nil
+			expect(@etiqueta.fibra).not_to be nil
+			expect(@etiqueta.proteina).not_to be nil
+			expect(@etiqueta.sal).not_to be nil
 		end
   end
 end
