0fdbdcabdc186b967563a7cd3cb5c4291a38e6a6
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-11-19 00:13:08 +0000

    añadiendo dependencias para guard

--- spec/etiqueta_spec.rb
+++ spec/etiqueta_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 require 'terminal-table'
 RSpec.describe Etiqueta do
-  it "has a version number" do
-    expect(Etiqueta::VERSION).not_to be nil
-  end
-  it "does something useful" do
-    expect(true).to eq(true)
-  end
-  before :each do
-                @etiqueta = InfoNutricional.new("Galletas integrales", 20, 12.6, 1.2, 70.0, 21.0, 7.3, 0.003, 9.9, 1.5, 0.0, 0.0, 7.5)
-        end
-  context "# Probando el constructor" do
+    it "has a version number" do
+        expect(Etiqueta::VERSION).not_to be nil
+    end
+    it "does something useful" do
+        expect(true).to eq(true)
+    end
+    before :each do
+        @etiqueta = InfoNutricional.new("Galletas integrales", 20, 12.6, 1.2, 70.0, 21.0, 7.3, 0.003, 9.9, 1.5, 0.0, 0.0, 7.5)
+    end
+    context "# Probando el constructor" do
         it "Espectativa de que existe un constructor" do
 			prueba = InfoNutricional.new("Galletas integrales", 20, 12.6, 1.2, 70.0, 21.0, 7.3, 0.003, 9.9, 1.5, 0.0, 0.0, 7.5)
         end
@@ -28,7 +28,7 @@ RSpec.describe Etiqueta do
 			expect(@etiqueta.sal).not_to be nil
 		end
 		it "Se almacena correctamente el nombre" do
-        	        expect(@etiqueta.nombre).to eq("Galletas integrales")
+        	    expect(@etiqueta.nombre).to eq("Galletas integrales")
 	        end
 		it "Se almacena correctamente la porción" do 
 			expect(@etiqueta.porcion).to eq(20)
@@ -66,46 +66,46 @@ RSpec.describe Etiqueta do
 		it "Se almacena correctamente la sal" do
 			expect(@etiqueta.sal).to eq(0.003)
                 end
-  end
-  context "# Métodos adicionales" do
-	it "Espectativa para método valor_energetico" do
-		expect(@etiqueta.valor_energetico).to eq(423)
-	end
-  end
-  context "# Métodos para mostrar por pantalla" do
-	  it "Espectativa para método to_s, tabla formateada" do
-		  filas = [["Valor energético","1771kJ/ 423kcal", "353kJ/ 84kcal","4.23%"], 
-	     ["Grasas totales\n -Saturadas\n -Monoinsaturada\n -Poliinsaturada", "12.6g\n1.2g\n9.9g\n1.5g", "2.52g\n0.24g\n1.98g\n0.3g", "3.6%\n1.2%\n-\n-"], 
-	     ["Hidratos de carbono\n -Azúcares\n -Polialcoholes\n -Almidón","70.0g\n21.0g\n0.0g\n0.0g","14.0g\n4.2g\n0.0g\n0.0g", "5.38%\n4.67%\n-\n-"], 
-	     ["Fibra","7.5g","1.5g","-"], 
-	     ["Proteina","7.3g","1.46g","2.92%"], 
-	     ["Sal","0.003g","0.0006g","0.01%"]]
-		tabla = Terminal::Table.new :title => "Galletas integrales", :headings => ['', 'Cantidad por 100g', "Cantidad por porción (20g)", "IR (por porción de 20g de producto)"], :rows => filas
-		expect(@etiqueta.to_s).to eq("#{tabla}")
-	  end
-  end
-  context "# Ingesta recomendada (IR)" do
-	it "Método ingesta recomendada kcal" do
-		expect(@etiqueta.ir_kcal).to eq(4.23)
-	end
-	it "Método ingesta recomendada grasa" do
-		expect(@etiqueta.ir_grasa).to eq(3.6)
-	end
-	it "Método ingesta recomendada grasa saturada" do
-		expect(@etiqueta.ir_grasa_saturada).to eq(1.2)
-	end
-	it "Método ingesta recomendada de hidratos de carbono" do
-		expect(@etiqueta.ir_carbohidratos).to eq(5.38)
-	end
-	it "Método ingesta recomendada de azúcar" do
-		expect(@etiqueta.ir_azucar).to eq(4.67)
-	end
-	it "Método ingesta recomendada de proteinas" do
-		expect(@etiqueta.ir_proteina).to eq(2.92)
-	end
-	it "Método ingesta recomendada de sal" do
-		expect(@etiqueta.ir_sal).to eq(0.01)
-	end	
-  end
+    end
+    context "# Métodos adicionales" do
+	    it "Espectativa para método valor_energetico" do
+		    expect(@etiqueta.valor_energetico).to eq(423)
+	    end
+    end
+    context "# Métodos para mostrar por pantalla" do
+	    it "Espectativa para método to_s, tabla formateada" do
+		    filas = [["Valor energético","1771kJ/ 423kcal", "353kJ/ 84kcal","4.23%"], 
+	        ["Grasas totales\n -Saturadas\n -Monoinsaturada\n -Poliinsaturada", "12.6g\n1.2g\n9.9g\n1.5g", "2.52g\n0.24g\n1.98g\n0.3g", "3.6%\n1.2%\n-\n-"], 
+	        ["Hidratos de carbono\n -Azúcares\n -Polialcoholes\n -Almidón","70.0g\n21.0g\n0.0g\n0.0g","14.0g\n4.2g\n0.0g\n0.0g", "5.38%\n4.67%\n-\n-"], 
+	        ["Fibra","7.5g","1.5g","-"], 
+	        ["Proteina","7.3g","1.46g","2.92%"], 
+	        ["Sal","0.003g","0.0006g","0.01%"]]
+		    tabla = Terminal::Table.new :title => "Galletas integrales", :headings => ['', 'Cantidad por 100g', "Cantidad por porción (20g)", "IR (por porción de 20g de producto)"], :rows => filas
+		    expect(@etiqueta.to_s).to eq("#{tabla}")
+	    end
+    end
+    context "# Ingesta recomendada (IR)" do
+	    it "Método ingesta recomendada kcal" do
+		    expect(@etiqueta.ir_kcal).to eq(4.23)
+	    end
+	    it "Método ingesta recomendada grasa" do
+		    expect(@etiqueta.ir_grasa).to eq(3.6)
+	    end
+	    it "Método ingesta recomendada grasa saturada" do
+		    expect(@etiqueta.ir_grasa_saturada).to eq(1.2)
+	    end
+	    it "Método ingesta recomendada de hidratos de carbono" do
+		    expect(@etiqueta.ir_carbohidratos).to eq(5.38)
+	    end
+	    it "Método ingesta recomendada de azúcar" do
+		    expect(@etiqueta.ir_azucar).to eq(4.67)
+	    end
+	    it "Método ingesta recomendada de proteinas" do
+		    expect(@etiqueta.ir_proteina).to eq(2.92)
+	    end
+        it "Método ingesta recomendada de sal" do
+            expect(@etiqueta.ir_sal).to eq(0.01)
+        end	
+    end
 end
 
