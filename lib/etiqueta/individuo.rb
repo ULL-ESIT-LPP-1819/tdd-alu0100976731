d135e6d2c29dbd84af183edefc8e7bfbeb43dd37
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-11-20 17:58:36 +0000

    Creando constructor para Individuo

--- lib/etiqueta/individuo.rb
+++ lib/etiqueta/individuo.rb
@@ -1,2 +1,8 @@
 class Individuo
+    def initialize(nombre, apellidos, edad, f_nacimiento, genero, ocupacion)
+        @nombre, @apellidos = nombre, apellidos
+        @edad, @f_nacimiento = edad, f_nacimiento
+        @genero = genero
+        @ocupacion = ocupacion
+    end
 end
--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 RSpec::describe Individuo do
+    context 'Método iniciales de clase Individuo' do
+        it 'El constructor funciona de forma adecuada' do
+            prueba = Individuo.new('Juan Jesús', 'Padrón Hernández', 21, '22/08/1997','Hombre', 'Estudiante')
+        end
+    end
 end
