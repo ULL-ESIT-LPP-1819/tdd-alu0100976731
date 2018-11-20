4478dd55a2076ae1b8e56bf547e2d78d3954f7f1
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-11-20 18:17:31 +0000

    Creando getters. Creando método to_s.

--- lib/etiqueta/individuo.rb
+++ lib/etiqueta/individuo.rb
@@ -1,8 +1,15 @@
 class Individuo
+    attr_reader :nombre, :apellidos, :edad, :f_nacimiento, :genero, :ocupacion
     def initialize(nombre, apellidos, edad, f_nacimiento, genero, ocupacion)
         @nombre, @apellidos = nombre, apellidos
         @edad, @f_nacimiento = edad, f_nacimiento
         @genero = genero
         @ocupacion = ocupacion
     end
+    def to_s
+        "INDIVIDUO\n-Nombre: #{@nombre}\n-Apellidos: #{@apellidos}\n-Edad: #{@edad}\n-Fecha de nacimiento: #{@f_nacimiento}\n-Género: #{@genero}\n-Ocupación: #{@ocupacion}"
+    end
 end
+
+#individuo1 = Individuo.new('Juan Jesús', 'Padrón Hernández', 21, '22/08/1997','Hombre', 'Estudiante')
+#puts individuo1
--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -2,8 +2,22 @@ require 'spec_helper'
 
 RSpec::describe Individuo do
     context 'Método iniciales de clase Individuo' do
+        before :each do
+            @individuo1 = Individuo.new('Juan Jesús', 'Padrón Hernández', 21, '22/08/1997','Hombre', 'Estudiante')
+        end
         it 'El constructor funciona de forma adecuada' do
             prueba = Individuo.new('Juan Jesús', 'Padrón Hernández', 21, '22/08/1997','Hombre', 'Estudiante')
+        end
+        it 'Comprobando getters' do
+            expect(@individuo1.nombre).to eq('Juan Jesús')
+            expect(@individuo1.apellidos).to eq('Padrón Hernández')
+            expect(@individuo1.edad).to eq(21)
+            expect(@individuo1.f_nacimiento).to eq('22/08/1997')
+            expect(@individuo1.genero).to eq('Hombre')
+            expect(@individuo1.ocupacion).to eq('Estudiante')
+        end
+        it 'Comprobando método to_s' do
+            expect(@individuo1.to_s).to eq("INDIVIDUO\n-Nombre: Juan Jesús\n-Apellidos: Padrón Hernández\n-Edad: 21\n-Fecha de nacimiento: 22/08/1997\n-Género: Hombre\n-Ocupación: Estudiante")
         end
     end
 end
