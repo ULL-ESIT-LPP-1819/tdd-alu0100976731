0d92b1a58c2ca170466e53b82f1953fd5ada6512
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-11-21 01:41:47 +0000

    Creando clase IndividuoPaciente, getters, y tests

--- lib/etiqueta/individuo.rb
+++ lib/etiqueta/individuo.rb
@@ -11,5 +11,16 @@ class Individuo
     end
 end
 
+class IndividuoPaciente < Individuo
+    attr_reader :peso, :talla, :c_cintura, :c_cadera
+    def initialize(nombre, apellidos, edad, f_nacimiento, genero, ocupacion, peso, talla, c_cintura, c_cadera, *opcional)
+        super(nombre, apellidos, edad, f_nacimiento, genero, ocupacion)
+        @peso = peso
+        @talla = talla
+        @c_cintura = c_cintura
+        @c_cadera = c_cadera
+    end
+end
 #individuo1 = Individuo.new('Juan Jesús', 'Padrón Hernández', 21, '22/08/1997','Hombre', 'Estudiante')
 #puts individuo1
+#peso, talla, c_cintura, c_cadera
--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -21,3 +21,37 @@ RSpec::describe Individuo do
         end
     end
 end
+
+RSpec::describe IndividuoPaciente do
+    before :each do
+        @individuo1 = IndividuoPaciente.new('Juan Jesús', 'Padrón Hernández', 21, '22/08/1997','Hombre', 'Estudiante', 85, 1.85, 90, 100)
+    end
+    context 'Métodos iniciales' do
+        it 'Existe un constructor' do
+            prueba = IndividuoPaciente.new('Juan Jesús', 'Padrón Hernández', 21, '22/08/1997','Hombre', 'Estudiante', 85, 1.85, 90, 100)
+        end
+        it 'Comprobando getters' do
+            expect(@individuo1.nombre).to eq('Juan Jesús')
+            expect(@individuo1.apellidos).to eq('Padrón Hernández')
+            expect(@individuo1.edad).to eq(21)
+            expect(@individuo1.f_nacimiento).to eq('22/08/1997')
+            expect(@individuo1.genero).to eq('Hombre')
+            expect(@individuo1.ocupacion).to eq('Estudiante')
+            expect(@individuo1.peso).to eq(85)
+            expect(@individuo1.talla).to eq(1.85)
+            expect(@individuo1.c_cintura).to eq(90)
+            expect(@individuo1.c_cadera).to eq(100)
+        end
+    end
+    context 'Comprobando clase, tipo y pertenencia a una jerarquía' do
+        it 'El objeto es una instancia de IndividuoPaciente'  do
+            expect(@individuo1).to be_instance_of(IndividuoPaciente)
+        end
+        it 'El objeto es de la clase IndividuoPaciente'  do
+            expect(@individuo1.class).to be(IndividuoPaciente)
+        end
+        it 'Pertenece a la jerarquía de clase de Individuo' do
+            expect(@individuo1).to be_kind_of(Individuo)
+        end
+    end
+end
