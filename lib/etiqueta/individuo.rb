7c48a94157ead9cd5a18b7f93f2c6ee5ca7ab9d6
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-12-06 19:43:37 +0000

    Documentación de las clases Individuo e IndividuoPaciente

--- lib/etiqueta/individuo.rb
+++ lib/etiqueta/individuo.rb
@@ -1,19 +1,52 @@
+# La clase Individuo representa la información de una persona.
+#
+# @author Juan Jesús Padrón Hernández  (mailto:alu0100976731@ull.edu.es)
 class Individuo
     attr_reader :nombre, :apellidos, :edad, :f_nacimiento, :genero, :ocupacion
+
+    # Inicializa el objeto con los parámetros indicados.
+    # @param nombre [String] Nombre de la persona.
+    # @param apellidos [String] Sus apellidos.
+    # @param edad [Numeric] Su edad.
+    # @param f_nacimiento [String] Su fecha de nacimiento.
+    # @param genero [String] Su género.
+    # @param ocupacion [String] A qué se dedica la persona.
     def initialize(nombre, apellidos, edad, f_nacimiento, genero, ocupacion)
         @nombre, @apellidos = nombre, apellidos
         @edad, @f_nacimiento = edad, f_nacimiento
         @genero = genero
         @ocupacion = ocupacion
     end
+
+    # Devuelve la cadena con la que se representará a la persona.
+    # @return [String] Cadena con los datos del objeto de la clase Individuo.
     def to_s
         "INDIVIDUO\n-Nombre: #{@nombre}\n-Apellidos: #{@apellidos}\n-Edad: #{@edad}\n-Fecha de nacimiento: #{@f_nacimiento}\n-Género: #{@genero}\n-Ocupación: #{@ocupacion}"
     end
 end
 
+# Clase que representa los datos de una persona con su valoración nutricional.
+# Representa a un paciente que está siendo tratado, y por tanto, se tienen sus medidas antropométricas.
+# Es hija de la clase _Individuo_.
+# Se ha hecho _Comparable_.
+#
+# @author Juan Jesús Padrón Hernández  (mailto:alu0100976731@ull.edu.es)
 class IndividuoPaciente < Individuo
     include Comparable
     attr_reader :n_historia, :peso, :talla, :c_cintura, :c_cadera
+
+    # Inicializa el objeto con los parámetros indicados.
+    # @param nombre [String] Nombre de la persona.
+    # @param apellidos [String] Sus apellidos.
+    # @param edad [Numeric] Su edad.
+    # @param f_nacimiento [String] Su fecha de nacimiento.
+    # @param genero [String] Su género.
+    # @param ocupacion [String] A qué se dedica la persona.
+    # @param n_historia [Numeric] Identificador de la historia médica.
+    # @param peso [Numeric] Peso de la persona.
+    # @param talla [Numeric] Su altura.
+    # @param c_cintura [Numeric] Circunferencia de su cintura.
+    # @param c_cadena [Numeric] Circunferencia de su cadera.
     def initialize(nombre, apellidos, edad, f_nacimiento, genero, ocupacion, n_historia, peso, talla, c_cintura, c_cadera, *opcional)
         super(nombre, apellidos, edad, f_nacimiento, genero, ocupacion)
         @n_historia = n_historia
@@ -22,9 +55,15 @@ class IndividuoPaciente < Individuo
         @c_cintura = c_cintura
         @c_cadera = c_cadera
     end
+
+    # Calcula el Índice de Masa Corporal del paciente.
+    # @return [Numeric] IMC de la persona.
     def imc
         (@peso/@talla**2).round(2)
     end
+
+    # Calcula el porcentaje de grasa corporal del paciente.
+    # @return [Numeric] Porcentaje de grasa corporal de la persona.
     def p_grasa
         if @genero == 'Hombre'
             (1.2*imc+0.23*@edad-10.8*1-5.4).round(2)
@@ -32,15 +71,21 @@ class IndividuoPaciente < Individuo
             (1.2*imc+0.23*@edad-5.4).round(2)
         end
     end
+
+    # Calcula la Relación Cintura Cadera del paciente.
+    # @return [Numeric] RCC de la persona.
     def rcc
         (c_cintura.to_f/c_cadera).round(2)
     end
+
+    # Representa la información del objeto IndividuoPaciente.
+    # @return [String] Cadena con la representación de la información personal y medidas antropométricas.
     def to_s
         super + "\n-Nº Historia: #{@n_historia}\n-Peso: #{@peso}\n-Talla: #{@talla}\n-Circ. Cintura: #{@c_cintura}\n-Circ. Cadera: #{@c_cadera}\n-IMC: #{imc}\n-RCC: #{rcc}"
     end
-    
+
+    # Define la clase como Comparable. Compara los alimentos por su IMC.
     def <=>(other)
         imc <=> other.imc
     end
-
 end
