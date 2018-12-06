baa8823cff2120b67af5ba2dd7ea57367019d033
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-12-06 19:47:10 +0000

    Documentación restante

--- lib/etiqueta.rb
+++ lib/etiqueta.rb
@@ -3,6 +3,7 @@ require "etiqueta/info_nutricional"
 require "etiqueta/lista"
 require "etiqueta/individuo"
 module Etiqueta
+  # Clase que gestiona errores.
   class Error < StandardError; end
   # Your code goes here...
 end
--- lib/etiqueta/version.rb
+++ lib/etiqueta/version.rb
@@ -1,3 +1,6 @@
+# Define el modulo Etiqueta
+# @author Juan Jesús Padrón Hernández  (mailto:alu0100976731@ull.edu.es)
 module Etiqueta
+  # Indica la versión de la gema.
   VERSION = "0.1.0"
 end
