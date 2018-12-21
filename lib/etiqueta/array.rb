abbbdbc870508adda7d604bedebd3d1448c7277c
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-12-21 05:22:07 +0000

    sort_for array

--- lib/etiqueta/array.rb
+++ lib/etiqueta/array.rb
@@ -1,17 +1,48 @@
 class Array
-  def sort_for
-    aux = [self[0]]
-    for i in (1..self.size)
-      for j in (0..aux.size)
-        if self[i] <= aux[j]
-          aux.insert(j,self[i])
-          break
-        end
-        if aux.size == j
-          aux.push(self[i])
-        end
-      end
+  def valor_energetico_for
+    aux = 0
+    for i in (0..self.size-1)
+      aux+=self[i].valor_energetico
     end
     aux
   end
+  def sort_for
+    aux = [self[0]]
+    for i in (1..self.size-1)
+			for j in (0..aux.size)
+				if aux.size == j
+          aux.push(self[i])
+        elsif self[i].valor_energetico_for <= aux[j].valor_energetico_for
+          aux.insert(j,self[i])
+          break
+        end
+      end
+    end
+		# Lista.new().insert_array(aux)
+		aux
+	end
+
+	def sort_each
+		aux = []
+		i = 0
+		self.each do |x|
+			if i!=0
+				j = 0
+				aux.each do |y|
+					if j == aux.size
+						aux.push(x)
+					elsif x<y
+						aux.insert(j,x)
+						break
+					end
+					j +=1
+				end
+			else
+				aux.push(x)
+			end
+			i+=1
+		end 
+		# Lista.new().insert_array(aux)
+		aux
+	end
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -41,4 +41,37 @@ RSpec::describe "Ordenación" do
       expect(@lista_individuos.sort).to eq([@individuo7, @individuo2, @individuo4, @individuo8, @individuo3, @individuo6, @individuo9, @individuo5, @individuo1, @individuo0])
     end
   end
+  context "Pruebas menus" do
+    before :all do
+      @alimento1 = InfoNutricional.new("Galletas integrales", 100, 12.6, 1.2, 70.0, 21.0, 7.3, 0.003, 9.9, 1.5, 0.0, 0.0, 7.5)
+		  @alimento2 = InfoNutricional.new("Leche entera", 250, 3.6, 2.5, 4.7, 4.7, 3.0, 0.13)
+		  @alimento3 = InfoNutricional.new("Oroweat 12 cereales y semillas", 80, 5.9, 0.8, 42, 5.5, 11.0, 0.98, 1.8, 3.3, 0.0, 36.0, 6.0)
+		  @alimento4 = InfoNutricional.new("Anchoas", 200, 13, 1.8, 0.5, 0.5, 25, 14.3)
+      @alimento5 = InfoNutricional.new("Chocolate 85%", 50, 46.0, 28.0, 22.0, 15.0, 12.0, 0.02, 14.0, 1.6, nil, nil, 15)
+      @alimento6 = InfoNutricional.new("Lentejas", 200, 1, 0, 40, 1, 23, 0.024, 0, 0, 0, nil, 17)
+      @alimento7 = InfoNutricional.new("Pecuga de pollo", 200, 3, 1, 0, 0, 31, 0.0074)
+      @alimento8 = InfoNutricional.new("Arroz blanco", 200, 1, 0, 75, 5, 8, 0)
+      @alimento9 = InfoNutricional.new("Hamburguesa", 200, 27, 13, 34, 9, 32, 0)
+      @alimento10 = InfoNutricional.new("Pizza barbacoa", 300, 10, 4, 36, 4, 12, 0)
+      @alimento11 = InfoNutricional.new("Gofio", 100, 3, 0, 81, 0, 11, 0)
+      @alimento12 = InfoNutricional.new("Garbanzos", 200, 6, 0, 61, 11, 19, 0.024, 1, 2, nil, nil, 17)
+      @menu_dietetico1 = [@alimento1, @alimento2, @alimento3, @alimento4]
+      @menu_dietetico2 = [@alimento5, @alimento2, @alimento6, @alimento7, @alimento8]
+      @menu_dietetico3 = [@alimento2, @alimento1, @alimento7, @alimento7, @alimento8, @alimento8]
+      @menu_dietetico4 = [@alimento1, @alimento9, @alimento10]
+      @menu_dietetico5 = [@alimento2, @alimento11, @alimento12, @alimento7, @alimento5]
+      @menu_dietetico6 = [@alimento1, @alimento2, @alimento3, @alimento4, @alimento5]
+      @menu_dietetico7 = [@alimento5, @alimento2, @alimento6, @alimento7, @alimento8, @alimento3]
+      @menu_dietetico8 = [@alimento2, @alimento1, @alimento7, @alimento7, @alimento8, @alimento8, @alimento12]
+      @menu_dietetico9 = [@alimento1, @alimento9, @alimento10, @alimento2]
+      @menu_dietetico0 = [@alimento2, @alimento11, @alimento12, @alimento7, @alimento5, @alimento10]
+      @menus = [@menu_dietetico0, @menu_dietetico1, @menu_dietetico2, @menu_dietetico3, @menu_dietetico4, @menu_dietetico5, @menu_dietetico6, @menu_dietetico7, @menu_dietetico8, @menu_dietetico9]
+    end
+    it "Pruebas sort_for" do
+      # p @menus.collect{|x| x.collect{|x| x.valor_energetico}.reduce(:+)}
+      # p @menus.collect{|x| x.collect{|x| x.valor_energetico}.reduce(:+)}.sort
+      # p @menus.sort_for
+      expect(@menus.sort_for).to eq([@menu_dietetico1, @menu_dietetico4, @menu_dietetico9, @menu_dietetico2, @menu_dietetico3, @menu_dietetico5, @menu_dietetico6, @menu_dietetico7, @menu_dietetico0, @menu_dietetico8])
+    end
+  end
 end
